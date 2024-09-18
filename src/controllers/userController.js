124498c6325328f93951d82d7225bd6197300d8a
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-09-18 13:53:06 -0300

    feat : controller de usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,24 @@
+import { userModel } from "../dao/models/userModel.js";
+import { UserManager } from "../manager/userManager.js";
 export class UserController {
-  static register = (req, res) => {
-    res.send("Register");
+  static register = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+      const existingUser = await UserManager.getByEmail(email);
+      if (existingUser) {
+        return res.status(409).json({ message: "User already exists" });
+      }
+
+      const user = await userModel.create(req.body);
+      //falta hashear contraseña
+      res.status(201).json({ message: "User created successfully", user });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static login = (req, res) => {
+    res.send("Login");
   };
 }
